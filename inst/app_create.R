38e66e0b26d82908e709258ea4b7a3b111c9c29b
Cord Huchzermeyer <huchzi@email.de>
2025-07-09 13:18:16 +0200

    Enabled adding detachments.

--- inst/app_create.R
+++ inst/app_create.R
@@ -5,7 +5,8 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textOutput("item_list"),
-      actionButton("add_tear", "+ Tear")
+      actionButton("add_tear", "+ Hufeisenriss"),
+      actionButton("add_detachment", "+ Netzhautablösung")
     ),
     mainPanel(
       htmlOutput("svg_image")
@@ -26,6 +27,19 @@ create_tear <- modalDialog(
   )
 )
 
+create_detachment <- modalDialog(
+  title = "Create detachment",
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      lapply(1:12, function (c) {
+      sliderInput(glue::glue("detachment_clock{c}"), glue::glue("{c} Uhr"),
+                  min = 0, max = 180, step = 10, value = 180)}),
+      actionButton("save_detachment", "Add")
+    )
+  )
+)
+
 server <- function(input, output, session) {
 
   fundus_items <- reactiveVal(list())
@@ -37,15 +51,29 @@ server <- function(input, output, session) {
         fundus_items()
       )
     )
+  })
 
+  observeEvent(input$save_detachment, {
+    radii <- sapply(1:12, function(x) { input[[glue::glue("detachment_clock{x}")]] })
+
+    print(radii)
+    fundus_items(
+      c(
+        list(list(type = "detachment", inner_radii = radii)),
+        fundus_items()
+      )
+    )
   })
 
   observeEvent(input$add_tear, {
     showModal(create_tear)
   })
 
+  observeEvent(input$add_detachment, {
+    showModal(create_detachment)
+  })
+
   output$svg_image <- renderUI({
-    req(!identical(fundus_items(), list()))
 
     stringr::str_c(ora_clip,
                    fundus_template,
