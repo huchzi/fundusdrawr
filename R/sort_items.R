b4e2f997df8a570baf79ed79e39472d8e3799594
Cord Huchzermeyer <huchzi@email.de>
2026-03-15 18:34:10 +0100

    Removed raster for drawing detachments.

--- R/sort_items.R
+++
@@ -1,6 +0,0 @@
-sort_items <- function(item_list) {
-  ord <- sapply(item_list, function(x) x$type) |>
-    factor(levels = c("detachment", "equatorial", "tear"), ordered = TRUE) |>
-    order()
-  item_list[ord]
-}
