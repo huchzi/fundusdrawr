08fb1f02922077f5672423dd6e4c41123e6e0083
huchzi <huchzi@email.de>
2026-03-31 07:21:14 +0200

    Create json for AI training.

---
+++ inst/json_for_schema.R
@@ -0,0 +1,7 @@
+[ { "type": "tear", "clock": 4, "eccentricity": 100, "size": "small" } ]
+
+[ { "type": "equatorial", "from": 3, "to": 5 }, { "type": "tear", "clock": 4, "eccentricity": 100, "size": "small" } ]
+
+[ { "type": "detachment", "path": [ { "cx": 342.8, "cy": 176.3188 }, { "cx": 275.7, "cy": 202.7188 }, { "cx": 236.1, "cy": 240.1188 }, { "cx": 239.4, "cy": 300.6188 }, { "cx": 249.3, "cy": 343.5188 }, { "cx": 328.5, "cy": 322.6188 }, { "cx": 365.9, "cy": 210.4188 } ] }, { "type": "tear", "clock": 3.5, "eccentricity": 100, "size": "medium" } ]
+
+[ { "type": "tear", "clock": 12, "eccentricity": 90, "size": "large" } ]
